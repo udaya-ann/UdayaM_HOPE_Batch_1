c5fa3872e3db892870c1104a4165f408fbc2e469
udaya-ann <annmariyammm85@gmail.com>
2026-04-08 10:17:17 +0530

    Time: 2 ms (91.09%), Space: 48 MB (11.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> l=new LinkedList<>();
-        int zerocount=0;
-        for(int n:nums){
-            if(n!=0)l.add(n);
-            else zerocount++;
-        }
-        for(int i=0;i<zerocount;i++)l.add(0);
-        int k=0;
-        for(int n:l){
-            nums[k]=n;
-            k++;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
         }
         
     }
