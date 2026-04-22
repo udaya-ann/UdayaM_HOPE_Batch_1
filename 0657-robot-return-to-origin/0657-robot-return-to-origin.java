2e5b2fe262397ef4b41cf583a31ac89f4fdd8d47
udaya-ann <annmariyammm85@gmail.com>
2026-04-22 09:40:39 +0530

    Time: 10 ms (6.93%), Space: 45.5 MB (84.26%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -4,11 +4,10 @@ class Solution {
         int lr=0;
         for(int i=0;i<moves.length();i++){
             if(moves.charAt(i)=='U') ud++;
-            if(moves.charAt(i)=='D') ud--;
-            if(moves.charAt(i)=='L') lr++;
-            if(moves.charAt(i)=='R') lr--;
+            else if(moves.charAt(i)=='D') ud--;
+            else if(moves.charAt(i)=='L') lr++;
+            else if(moves.charAt(i)=='R') lr--;
         }
-        if(ud==0 && lr==0) return true;
-        return false;
+        return ud==0 && lr==0;
     }
 }
