9881f96b672a4c16538b7d4c93f3ca8da49dbe0b
udaya-ann <annmariyammm85@gmail.com>
2026-09-15 11:55:16 +0530

    Time: 1 ms (59.13%), Space: 42.3 MB (84.09%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans=(n&1)|(ans<<1);
+            n=n>>>1;
+        }
+        return ans;
+    }
+}
