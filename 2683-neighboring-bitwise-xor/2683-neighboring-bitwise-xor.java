3877d215d401741efe361c80cd2e156b446809bb
udaya-ann <annmariyammm85@gmail.com>
2026-09-16 22:12:38 +0530

    Time: 2 ms (100%), Space: 199 MB (65.5%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor = 0;
+
+        for (int num : derived) {
+            xor ^= num;
+        }
+
+        return xor == 0;
+    }
+}
