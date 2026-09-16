d20b7fc1956528bf4126dc7bc46eaf34bf1352f3
udaya-ann <annmariyammm85@gmail.com>
2026-09-16 10:32:28 +0530

    Time: 2 ms (76.86%), Space: 44.9 MB (96.92%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+       
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(int num:nums){
+                count+=(num>>i)&1;
+            }
+            if(count%3!=0){
+                ans=ans|(1<<i);
+            }
+        }
+        return ans;
+    }
+}
