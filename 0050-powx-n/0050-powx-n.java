e7d831eae5ca93facf31ff62b42d1fabacafd5ac
udaya-ann <annmariyammm85@gmail.com>
2026-09-18 09:53:56 +0530

    Time: 0 ms (100%), Space: 48.3 MB (21.5%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        return power(x,n);
+    }
+    public double power(double x, int n){
+        if(n==0)return 1;
+        double p=power(x,n/2);
+        if(n%2==0) return p*p;
+        else return p*p*x;
     }
 }
